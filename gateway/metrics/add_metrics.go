0a3294bff31f56095b7e5f2ff7467619aa92d014
zyy <1755635168@qq.com>
2024-02-13 20:13:37 +0800

    version2.7

--- gateway/metrics/add_metrics.go
+++ gateway/metrics/add_metrics.go
@@ -97,7 +97,7 @@ func AddMetricsHandler(handler http.HandlerFunc, prometheusQuery PrometheusQuery
 			//CPU和memory
 			//ns1 := functions[0].Namespace
 			//q1 := fmt.Sprintf(`sum(pod_cpu_usage_seconds_total{function_name=~".*.%s"}) by (function_name)`, ns1)
-			q1 := fmt.Sprintf(`sum by(container, namespace) (container_cpu_usage_seconds_total{image!="",namespace="openfaas-fn", container!="POD"})`)
+			q1 := fmt.Sprintf(`sum by(container, namespace) (irate(container_cpu_usage_seconds_total{image!="",namespace="openfaas-fn", container!="POD"}[5m])*100)`)
 			results1, err1 := prometheusQuery.Fetch(url.QueryEscape(q1))
 			if err1 != nil {
 				// log the error but continue, the mixIn will correctly handle the empty results.
